9806fb16d693040801a37ae302f6f1ccbebf6bc5
Mattia Evangelisti <71966068+MadMatti@users.noreply.github.com>
2023-05-06 18:25:10 +0200

    started

--- dataset.py
+++ dataset.py
@@ -1,53 +1,150 @@
-import os
-import numpy as np
+from typing import List, Tuple, Union
+import h5py
+
+from torch.utils.data import Dataset as TorchDataset
+from torch_geometric.data import Dataset as GeometricDataset, Data
 import torch
-from torch_geometric.data import Data
-from torch_geometric.nn import knn_graph
+import torch_geometric.utils as utils
+
+import networkx as nx
+import numpy as np
+from scipy.spatial.distance import pdist, squareform
 
 
-path_dataset = '/Users/mattiaevangelisti/Documents/KITTI'
-save_path = '/Users/mattiaevangelisti/Documents/KITTI/KITTI_graph'
-os.makedirs(save_path, exist_ok=True)
+def knn_graph(data, k):
+    """
+    Construct a kNN graph from the given data
+    :param data: point cloud data
+    :param k: number of nearest neighbors
+    :return: networkx graph
+    """
+    # Compute pairwise distance matrix
+    D = pdist(data)
+    #D = np.exp(-D)
+    D = 1/(1+D)
+    D = squareform(D)
 
+    # Sort distance matrix in ascending order and get indices of points
+    idx = np.argsort(D, axis=1)
 
-def load_point_cloud(filename):
-    point_cloud = np.fromfile(filename, dtype=np.float32).reshape(-1, 4)
-    return point_cloud[:, :3]
+    # Construct kNN graph, use 3D points as node features
+    G = nx.Graph()
+    for i in range(data.shape[0]):
+        for j in idx[i, 1:k+1]:
+            G.add_edge(i, j, weight=D[i, j])
+    for i in range(data.shape[0]):
+        G.nodes[i]['x'] = data[i]
 
-def create_knn_graph(point_cloud, k=10):
-    point_cloud_tensor = torch.tensor(point_cloud, dtype=torch.float)
-    edge_index, edge_weight = knn_graph(point_cloud_tensor, k=k, batch=None, loop=False)
-    edge_weight = torch.sqrt(edge_weight)
+    return G
 
-    graph_data = Data(x=point_cloud_tensor, edge_index=edge_index, edge_attr=edge_weight)
-
-    return graph_data
-
-def process_and_save_graphs(path_dataset, save_path, k=10):
-    point_cloud_path = os.path.join(path_dataset, 'data_object_velodyne/training/velodyne')
-    point_cloud_files = [os.path.join(point_cloud_path, f) for f in os.listdir(point_cloud_path) if f.endswith('.bin')]
-
-    for i, file in enumerate(point_cloud_files):
-        point_cloud = load_point_cloud(file)
-        graph_data = create_knn_graph(point_cloud, k=k)
-
-        save_file_path = os.path.join(save_path, f'kitti_graph_{i}.pt')
-        torch.save(graph_data, save_file_path)
-
-    print(f"Processed and saved {len(point_cloud_files)} graphs to {save_path}")
-
-def load_kitti_graphs(save_path):
-    graph_files = [os.path.join(save_path, f) for f in os.listdir(save_path) if f.endswith('.pt')]
-    kitti_graphs = [torch.load(file) for file in graph_files]
-    return kitti_graphs
-
-
-def main():
-    process_and_save_graphs(path_dataset, save_path, k=10)
-
-
-if __name__ == '__main__':
-    main()
+class Dataset(GeometricDataset):
+    def __init__(self, path=None):
+        """
+        Initializes the dataset
+        Parameters
+        ----------
+        data : torch.Tensor
+            The data of the dataset
+        target : torch.Tensor
+            The target of the dataset
+        """
+        self.path = path
+        self.data = []
+        self.label = []
+        
+        super(Dataset, self).__init__(path)
     
+    def processed_file_names(self) -> str | List[str] | Tuple:
+        return ['data.pt', 'label.pt']
+
+    def get_class_weights(self):
+        """
+        Get the weights for each class
+        Returns
+        -------
+        torch.Tensor
+            The weights for each class
+        """
+
+        # Compute the weights
+        weights = 1 / np.sum(self.label, axis=0)
+
+        # Normalize the weights
+        weights = weights / np.sum(weights)
+
+        # Convert to tensor
+        weights = torch.Tensor(weights)
+
+        return weights
+    
+    def process(self):
+        if self.path is None:
+            return
+        
+        path = self.path + '/train.h5'
+        
+        import os
+        if os.path.exists(path + '.cache'):
+            # Load from cache
+            print('Loading from cache...')
+            import pickle
+            with open(path + '.cache', 'rb') as f:
+                self.data, self.label = pickle.load(f)
+            
+        # Load from file
+        f = h5py.File(path, 'r')
+
+        # convert to adjacency matrix
+        for i in range(len(f['data'])):
+            item = f['data'][i]
+
+            # Downsample the point cloud
+            item = item[::4, :]
+
+            # Convert the point cloud to a graph where each node represents a point and each edge represents the distance between two points
+            G = knn_graph(item, 1)
+
+            # Convert the graph to an adjacency matrix
+            #A = nx.adjacency_matrix(G).todense().astype(np.float32)
+
+            # Add 1 dimension for the channel
+            #A = np.expand_dims(A, axis=0)
+
+            # Convert to torch geometric data
+            A = utils.from_networkx(G)
+
+            # Add label
+            A.y = torch.tensor(f['label'][i], dtype=torch.long)
+
+            # Convert the label to a one-hot vector
+            label = np.zeros(10)
+            label[f['label'][i]] = 1
+            self.label.append(label)
+
+            # Plot the adjacency matrix
+            #import matplotlib.pyplot as plt
+            #plt.imshow(A)
+            #plt.title(f['label'][i])
+            #plt.show()
+
+            self.data.append(A)
+
+        f.close()
+
+        # Save to cache
+        import pickle
+        with open(path + '.cache', 'wb') as f:
+            pickle.dump((self.data, self.label), f)
+
+        # Print the number of items
+        print('Number of items:', len(self.data))
+
+        # Print the class distribution
+        print('Class distribution:', np.sum(self.label, axis=0))
 
     
+    def len(self):
+        return len(self.data)
+    
+    def get(self, idx):
+        return self.data[idx]
---
+++ model.py
@@ -0,0 +1,173 @@
+from torch import nn
+from torch_geometric import nn as gnn
+from torch.functional import F
+
+class Yofoold(nn.Module):
+    """
+    Yofo model
+        YO, Find Orientation
+    """
+
+    def __init__(self, n_classes):
+        super(Yofoold, self).__init__()
+        self.n_classes = n_classes
+        self.deeply_connected = nn.Sequential(
+            # input
+            nn.BatchNorm2d(1),
+            nn.Dropout2d(p=0.1),
+            nn.Conv2d(1, 16, kernel_size=(3, 3), stride=(1, 1), padding=1),
+
+            # /2
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=(2, 2), stride=(2, 2), padding=0),
+            nn.BatchNorm2d(16),
+            nn.Dropout2d(p=0.1),
+            nn.Conv2d(16, 16, kernel_size=(3, 3), stride=(1, 1), padding=1),
+
+            # /2
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=(2, 2), stride=(2, 2), padding=0),
+            nn.BatchNorm2d(16),
+            nn.Dropout2d(p=0.1),
+            nn.Conv2d(16, 16, kernel_size=(3, 3), stride=(1, 1), padding=1),
+
+            # /2
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=(2, 2), stride=(2, 2), padding=0),
+            nn.BatchNorm2d(16),
+            nn.Dropout2d(p=0.1),
+            nn.Conv2d(16, 16, kernel_size=(3, 3), stride=(1, 1), padding=1),
+
+            # /2
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=(2, 2), stride=(2, 2), padding=0),
+            nn.BatchNorm2d(16),
+            nn.Dropout2d(p=0.1),
+            nn.Conv2d(16, 16, kernel_size=(3, 3), stride=(1, 1), padding=1),
+
+            # /2
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=(2, 2), stride=(2, 2), padding=0),
+            nn.BatchNorm2d(16),
+            nn.Dropout2d(p=0.1),
+            nn.Conv2d(16, 16, kernel_size=(3, 3), stride=(1, 1), padding=1),
+
+            # /2
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=(2, 2), stride=(2, 2), padding=0),
+            nn.BatchNorm2d(16),
+            nn.Dropout2d(p=0.1),
+            nn.Conv2d(16, 16, kernel_size=(3, 3), stride=(1, 1), padding=1),
+
+            # flatten
+            nn.ReLU(),
+            nn.BatchNorm2d(16),
+            nn.Dropout2d(p=0.3),
+            nn.Flatten(),
+            nn.Linear(8*8*16, 32),
+
+            # deep
+            nn.BatchNorm1d(32),
+            nn.ReLU(),
+            nn.Linear(32, n_classes),
+
+            # output
+            nn.Softmax(dim=1)
+        )
+        self.float()
+
+    def forward(self, x):
+        x = self.deeply_connected(x)
+        return x
+
+class Yofo(nn.Module):
+    """
+    Yofo model
+        YO, Find Orientation
+    """
+
+    def __init__(self, n_classes):
+        super(Yofo, self).__init__()
+        self.n_classes = n_classes
+        self.deeply_connected = nn.Sequential(
+            # input
+            nn.Conv2d(1, 64, kernel_size=(3, 3), stride=(1, 1), padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=(4, 4), stride=(4, 4), padding=0),
+            nn.LayerNorm((64, 128, 128)),
+
+            nn.Conv2d(64, 32, kernel_size=(5, 5), stride=(1, 1), padding=2),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=(4, 4), stride=(4, 4), padding=0),
+            nn.LayerNorm((32, 32, 32)),
+
+            nn.Conv2d(32, 16, kernel_size=(3, 3), stride=(1, 1), padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=(4, 4), stride=(4, 4), padding=0),
+            nn.LayerNorm((16, 8, 8)),
+
+            nn.Conv2d(16, 1, kernel_size=(3, 3), stride=(1, 1), padding=1),
+            nn.ReLU(),
+            nn.LayerNorm((1, 8, 8)),
+
+            # flatten
+            nn.Flatten(),
+            nn.Linear(8*8, 32),
+            nn.ReLU(),
+            nn.LayerNorm(32),
+
+            # deep
+            nn.Linear(32, self.n_classes),
+
+            # output
+            nn.Sigmoid()
+        )
+        self.float()
+
+    def forward(self, x):
+        x = self.deeply_connected(x)
+        return x
+    
+class GraphClassifier(nn.Module):
+    def __init__(self, hidden_dim, output_dim):
+        super(GraphClassifier, self).__init__()
+
+        self.gnn1 = gnn.GCNConv(-1, hidden_dim)
+        self.gnn2 = gnn.GCNConv(hidden_dim, hidden_dim)
+        self.gnn3 = gnn.GCNConv(hidden_dim, output_dim)
+
+        #self.classifier = nn.Sequential(
+        #    nn.Linear(hidden_dim, hidden_dim),
+        #    nn.ReLU(),
+        #    nn.Linear(hidden_dim, output_dim),
+        #    nn.Softmax(dim=1)
+        #s)
+    
+    def forward(self, x):
+        x, edge_index, batch = x.x, x.edge_index, x.batch
+
+        print(x.shape)
+        print(edge_index.shape)
+
+        # Embedding
+        x = self.gnn1(x, edge_index)
+        x = F.relu(x)
+        x = self.gnn2(x, edge_index)
+        x = F.relu(x)
+        x = self.gnn3(x, edge_index)
+        x = F.softmax(x, dim=1)
+
+        # Pooling
+        #x = gnn.global_mean_pool(x, batch)
+
+        # Classification
+        #x = self.classifier(x)
+        return x
+
+class GraphSage(nn.Module):
+    '''GraphSAGE'''
+    def __init__(self, dim_in, dim_h, dim_out):
+        super(GraphSage, self).__init__()
+        self.conv1 = gnn.SAGEConv(dim_in, dim_h)
+        self.conv2 = gnn.SAGEConv(dim_h, dim_out)
+        self.optim
